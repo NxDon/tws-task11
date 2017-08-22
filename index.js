ee08e395f67011009b207cefe7c073cb4e15aaee
NxDon <645104928@qq.com>
2017-08-22 22:09:19 +0800

    add readme and remove node_modules

--- index.js
+++ index.js
@@ -1,40 +1,112 @@
 "use strict";
-var	query = require('cli-interact');
+var query = require('cli-interact');
 
-function addStudent() {
-
+function addStudent(studentInfo, infoList) {
+    var infoArr = studentInfo.split(',').join("：").split("：");
+    var [name, id, className] = infoArr;
+    infoList.push({
+        name,
+        id:parseFloat(id),
+        className:parseFloat(className),
+        math: parseFloat(infoArr[4]),
+        chinese: parseFloat(infoArr[6]),
+        english: parseFloat(infoArr[8]),
+        programming: parseFloat(infoArr[10])
+    })
+    console.log(`学生${name}的成绩被添加`);
+    return infoList;
 }
 
-function makeGradeList() {
-
+function formatToString(studentsGrades, mean, median) {
+    let title = `
+        成绩单
+姓名|数学|语文|英语|编程|平均分|总分 
+========================`;
+    let studentsGrade = '';
+    studentsGrades.forEach((obj) => {
+        studentsGrade += `\n${obj.name}|${obj.math}|${obj.chinese}|${obj.english}|${obj.programming}|${obj.mean}|${obj.total}`;//tober
+    })
+    let bottom = `\n========================
+全班总分平均数：${mean}
+全班总分中位数：${median}`;
+    return `${title}${studentsGrade}${bottom}`;
 }
 
-function isLeigitInfo(studentInfo) {
-    let reg = new RegExp(/^[\u4e00-\u9fa5]+,\d+,\d+,数学：\d+,语文：\d+,英语：\d+,编程：\d+$/)
+function getMedian(totalGrades) {
+    let length = totalGrades.length;
+    if (length % 2 === 0) {
+        return (totalGrades[length / 2 - 1] + totalGrades[length / 2]) / 2;
+    }
+    return totalGrades[Math.floor(length / 2)]
+}
+
+function getClassMeanAndMedian(infoLists) {
+    let totalGrades = [];
+    infoLists.forEach((obj) => {
+        totalGrades.push(obj.math);
+        totalGrades.push(obj.chinese);
+        totalGrades.push(obj.english);
+        totalGrades.push(obj.programming);
+    });
+    totalGrades = totalGrades.sort((a, b) => a - b);
+    let mean = totalGrades.reduce((a, b) => a + b) / totalGrades.length;
+    let median = getMedian(totalGrades);
+    return [mean, median];
+}
+function getSelectedGrade(ids, infoLists) {
+    let results = [];
+    let studentID = ids.split(',').map((elem) => parseInt(elem));
+    infoLists.forEach((obj) => {
+        if (studentID.includes(obj.id)) {
+            let total = parseFloat(obj.math) + parseFloat(obj.chinese) + parseFloat(obj.english) + parseFloat(obj.programming);
+            let mean = total / 4;
+            results.push({
+                name: obj.name,
+                math: obj.math,
+                chinese: obj.chinese,
+                english: obj.english,
+                programming: obj.programming,
+                mean,
+                total
+            })
+        }
+    });
+    return results;
+}
+
+function printGradeList(ids, infoLists) {
+        let studentsGrades = getSelectedGrade(ids, infoLists);
+    let [mean, median] = getClassMeanAndMedian(infoLists);
+    let result = formatToString(studentsGrades, mean, median);
+    console.log(result);
+}
+
+function isLegitInfo(studentInfo) {
+    let reg = new RegExp(/^[\u4e00-\u9fa5]+,\d+,\d+,\d+,\d+,\d+,\d+$/)
     return reg.test(studentInfo);
 }
 
 function main() {
-
-    while (true){
+    var infoList = [];
+    while (true) {
         console.log("1. 添加学生\n2. 生成成绩单\n3. 退出");
         let choice = query.getNumber("请输入你的选择（1～3）：");
         console.log(choice);
         switch (choice) {
             case 1:
-                while (true){
-                    let studentInfo = query.question("请输入学生信息（格式：姓名, 学号, 班级, 学科: 成绩, ...），按回车提交：");
-                    if(isLeigitInfo(studentInfo)){
+                while (true) {
+                    var studentInfo = query.question("请输入学生信息（格式：姓名, 学号, 班级, 数学成绩，语文成绩，英语成绩，编程成绩），按回车提交：\n");
+                    if (isLegitInfo(studentInfo)) {
                         break;
                     }
                     console.log("请按正确的格式输入（格式：姓名, 学号, 学科: 成绩, ...）：");
                     continue;
                 }
-
-                var studentInfos = addStudent(studentInfo);
+                addStudent(studentInfo, infoList);
                 break;
             case 2:
-                makeGradeList(studentNum);
+                var studentIDs = query.question("请输入要打印的学生的学号（格式： 学号, 学号,...），按回车提交：");
+                printGradeList(studentIDs, infoList);
                 break;
             case 3:
                 console.log('quitting');
@@ -50,9 +122,13 @@ function main() {
 main();
 
 
-
-
 module.exports = {
-    isLeigitInfo:isLeigitInfo,
-    main:main
+    isLegitInfo,
+    main,
+    addStudent,
+    makeGradeList: printGradeList,
+    getMeanAndMedian: getClassMeanAndMedian,
+    formatToString,
+    getStudentsGrade: getSelectedGrade,
+    getMedian,
 }
--- spec/main_spec.js
+++ spec/main_spec.js
@@ -9,25 +9,101 @@ chai.use(sinonChai);
 
 var index = require("../index.js");
 
-//
-// describe("测试isLegit函数", function(){
-//     sinon.spy(console, 'log');
-//
-//     it("测试输入学生信息是否正确", function(){
-//         var input = "段宇,12,2,数学：99,语文：88,英语：88,编程：99";
-//         var result = index.isLeigitInfo(input);
-//
-//         expect(result).to.equal(true);
-//     });
-//
-// });
-    describe("main函数测试", function(){
-        sinon.spy(console, 'log');
 
-        it("3退出", function(){
-            index.main();
-            let result = console.log.calledWith("quitting");
-            expect(result).to.equal(true);
-        });
+describe("测试函数", function () {
+    it("测试输入学生信息是否正确", function () {
+        var input = "段宇,12,22,23,42,124,34";
+        var result = index.isLegitInfo(input);
+        expect(result).to.equal(true);
+    });
+
+    it("测试添加学生信息到列表", function () {
+        let input = "段宇,12,2,数学：99,语文：88,英语：88,编程：99";
+        let infoList = [];
+        let result = index.addStudent(input, infoList);
+
+        //设置deep标记，然后使用equal和property断言。该标记可以让其后的断言不是比较对象本身，而是递归比较对象的键值对
+        expect(infoList).to.deep.equal([{
+            name: '段宇',
+            id: 12,
+            className: 2,
+            math: 99,
+            chinese: 88,
+            english: 88,
+            programming: 99
+        }])
+    });
+});
+
+describe("printGradeList子函数测试", function () {
+    let listInfo = [{
+        name: '段宇',
+        id: 1,
+        className: 2,
+        math: 88,
+        chinese: 88,
+        english: 88,
+        programming: 88
+    },{
+        name: '段天',
+        id: 2,
+        className: 2,
+        math: 88,
+        chinese: 88,
+        english: 88,
+        programming: 88
+    },{
+        name: '段昊',
+        id: 3,
+        className: 2,
+        math: 100,
+        chinese: 100,
+        english: 100,
+        programming: 100
+    }];
+
+    it("makeGradeList打印出学生成绩", function () {
+        let makeGradeList = index.makeGradeList;
+        let studentsIDs = '1,2';
+        let results = `
+        成绩单
+姓名|数学|语文|英语|编程|平均分|总分 
+========================
+段宇|88|88|88|88|88|352
+段天|88|88|88|88|88|352
+========================
+全班总分平均数：92
+全班总分中位数：88`;
+        sinon.spy(console, 'log');
+        makeGradeList(studentsIDs, listInfo);
+        expect(console.log).to.be.calledWith(results);
+    });
+
+    it("getStudentsGrade返回由学生成绩对象构成的数组", function () {
+        let result = index.getStudentsGrade('1,2', listInfo);
+        expect(result).to.be.deep.equal([{
+            name: "段宇", math: 88, chinese: 88, english: 88, programming: 88, mean: 88, total: 352
+        }, {
+            name: "段天", math: 88, chinese: 88, english: 88, programming: 88, mean: 88, total: 352
+        }])
+    })
+
+    it("getMeanAndMedian应该返回一个数组包含两个值，平均数和中位数", function () {
+        let result = index.getMeanAndMedian(listInfo);
+        expect(result).to.be.deep.equal([92, 88])
+    });
+
+    it("formatToString应该调用console.log函数，打印results", function () {
 
     });
+
+    it("getMedian应该返回数组中位数", function () {
+        let arr1 = [1, 2, 3, 4, 5];
+        let arr2 = [1, 2, 3, 4, 5, 6];
+        expect(index.getMedian(arr1)).to.be.equal(3);
+        expect(index.getMedian(arr2)).to.be.equal(3.5);
+    })
+
+})
+
+
